48aa31901d8450005f6348141b9f69abbaa493f4
luizsant <luizsant@gmail.com>
2024-05-19 19:27:23 -0300

    novo layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
+  // Função de animação fade-in
   const elementsToAnimate = document.querySelectorAll(".fade-in");
 
   const observerOptions = {
@@ -18,4 +18,23 @@ document.addEventListener("DOMContentLoaded", function () {
   elementsToAnimate.forEach((element) => {
     observer.observe(element);
   });
+
+  // Carrossel automático
+  const carousel = document.querySelector(".testimonial-carousel-inner");
+  const cards = document.querySelectorAll(".testimonial-card");
+  let index = 0;
+  const numCardsVisible =
+    window.innerWidth >= 1024 ? 3 : window.innerWidth >= 768 ? 2 : 1;
+
+  function moveCarousel() {
+    index += numCardsVisible; // Mover conforme o número de cards visíveis
+    if (index >= cards.length) {
+      index = 0;
+    }
+    carousel.style.transform = `translateX(${
+      -index * (100 / numCardsVisible)
+    }%)`;
+  }
+
+  setInterval(moveCarousel, 3000); // Muda o card a cada 3 segundos
 });
